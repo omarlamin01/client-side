9bd45667c9cefa1e8a957bcc804424d3801b80f4
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-02-01 11:44:25 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Join from './components/Join';
 import Chat from './components/Chat';
 
